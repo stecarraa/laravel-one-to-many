fcc40506ac4bef7045e55b910acec35ea97c4766
stecarraa <stefano@caratozzolo.me>
2022-09-21 16:37:26 +0200

    feat:  add foreign user_id in migration and prepare the seed

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 use App\Models\Post;
+use App\User;
 
 class PostsTableSeeder extends Seeder
 {
@@ -14,13 +15,18 @@ class PostsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $users = User::all();
+
         for($i = 0; $i < 50; $i++) {
             $newPost = new Post();
+
+            $newPost->user_id = $faker->randomElement($users)->id;
             $newPost->author = $faker->name();
             $newPost->title = $faker->sentence(3);
             $newPost->post_content = $faker->text(500);
             $newPost->post_image = $faker->imageUrl(360, 360, 'animals', true);
             $newPost->post_date = $faker->dateTime();
+            
             $newPost->save();
         }
     }
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $mySelf = new User();
+        $mySelf->name = 'stefano';
+        $mySelf->email = 'stecarraa95@gmail.com';
+        $mySelf->password = Hash::make('CiaoSilvia1');
+        $mySelf->save();
+
+        for($i = 0; $i < 10; $i++) {
+            $newUser = new User();
+            $newUser->name = $faker->userName();
+            $newUser->email = $faker->unique()->email();
+            $newUser->password = Hash::make($faker->password());
+            $newUser->save();
+        }
+    }
+    
+}
